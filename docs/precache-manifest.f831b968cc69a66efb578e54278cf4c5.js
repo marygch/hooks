14b85d3f6478a5fb37e84a9359640fca3d8c3faa
marygch <maria.r.garcia.ch@gmail.com>
2020-09-16 23:20:29 -0500

    build
    Signed-off-by: marygch <maria.r.garcia.ch@gmail.com>

---
+++ docs/precache-manifest.f831b968cc69a66efb578e54278cf4c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c208f98588411e294502cb3cae889ada",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab65004e0c1d0f58c7a9",
+    "url": "/static/css/main.8df6d38d.chunk.css"
+  },
+  {
+    "revision": "6505eeb413a487158288",
+    "url": "/static/js/2.de2a7863.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.de2a7863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab65004e0c1d0f58c7a9",
+    "url": "/static/js/main.c38dffed.chunk.js"
+  },
+  {
+    "revision": "18a8604375689c09671b",
+    "url": "/static/js/runtime-main.952f45a5.js"
+  }
+]);
